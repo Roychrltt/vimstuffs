6d6f690ad01dcf3fd636f794c42f07c6192bdb9c
Charlotte Roy <roychrltt@chacha.home>
2024-08-16 17:26:33 +0200

    a script to make updates

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cp ~/.vim/bundle/vim-snippets/UltiSnips/cpp.snippets ~/vimstuffs/.
+cp ~/.vim/bundle/vim-snippets/UltiSnips/c.snippets ~/vimstuffs/.
+cp ~/.zshrc ~/vimstuffs/zshrc
+cp ~/.vimrc ~/vimstuffs/vimrc
+git add .
