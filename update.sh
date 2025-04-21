054ffe82725e3fd6ca50d47973cb6c6e99b034c6
Charlotte <roychrltt@gmail.com>
2025-04-21 22:22:09 +0200

    Chacha gogogo

--- update.sh
+++ update.sh
@@ -2,6 +2,7 @@
 
 cp ~/.vim/bundle/vim-snippets/UltiSnips/cpp.snippets ~/vimstuffs/.
 cp ~/.vim/bundle/vim-snippets/UltiSnips/c.snippets ~/vimstuffs/.
+cp ~/.vim/bundle/vim-snippets/UltiSnips/go.snippets ~/vimstuffs/.
 cp ~/.zshrc ~/vimstuffs/zshrc
 cp ~/.vimrc ~/vimstuffs/vimrc
 git add .
