834081b74e7dee7f1f6ba27fd1916dde1912918d
Xiaoyun Xu <xiaxu@f1r8s7.clusters.42paris.fr>
2025-10-27 15:54:55 +0100

    .

--- gitall.sh
+++ gitall.sh
@@ -3,6 +3,7 @@
 cp ~/.vim/bundle/vim-snippets/UltiSnips/cpp.snippets ~/vimstuffs/.
 cp ~/.vim/bundle/vim-snippets/UltiSnips/c.snippets ~/vimstuffs/.
 cp ~/.vim/bundle/vim-snippets/UltiSnips/go.snippets ~/vimstuffs/.
+cp ~/.vim/bundle/vim-snippets/UltiSnips/python.snippets ~/vimstuffs/.
 cp ~/.zshrc ~/vimstuffs/zshrc
 cp ~/.vimrc ~/vimstuffs/vimrc
 
